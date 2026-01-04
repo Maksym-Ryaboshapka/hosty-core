312565c05ec90ca53bf783a178c4d49f4cdbef79
Maksym-Ryaboshapka <testbs777888@gmail.com>
2026-01-04 11:20:37 +0200

    init

---
+++ src/main/java/org/example/hostycore/HostyCoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.hostycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostyCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HostyCoreApplication.class, args);
+  }
+
+}
